6bee3e622a30f524bd7899d2334008d6fd11ad83
Tsalsb <timothy.salsberry+git@gmail.com>
2015-10-09 23:15:59 -0700

    Create SolidStateErase.py
    The First Draft

---
+++ SolidStateErase.py
@@ -0,0 +1,21 @@
+import subprocess,getpass
+import os, sys
+import errno
+import functools
+import Tkinter, tkFileDialog
+root = Tkinter.Tk()
+#Starting path of the file explorer for Tkinter
+myPath = tkFileDialog.askdirectory(parent=root, initialdir="/",
+title='Directory to destroy')
+
+password = getpass.getpass()
+#proc = subprocess.Popen(
+#  ['sudo','-p','','-S','/etc/init.d/test','restart'],
+#   stdin=subprocess.PIPE)
+proc = subprocess.Popen(
+    ['sudo','rm','','-rf',(myPath+"/"),],
+    stdin=subprocess.PIPE)
+proc.stdin.write(password+'\n')
+proc.stdin.close()
+proc.wait()
+
