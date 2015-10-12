baadcb689ab7d1aeb3cd363f93d6eb5c6c4b2933
Tsalsb <timothy.salsberry+git@gmail.com>
2015-10-11 22:46:24 -0700

    Update SolidStateErase.py

--- SolidStateErase.py
+++ SolidStateErase.py
@@ -18,4 +18,38 @@ proc = subprocess.Popen(
 proc.stdin.write(password+'\n')
 proc.stdin.close()
 proc.wait()
+######################################################
+# This is the begining of the Sudo Random Text Generator#
+
+
+# First we get free Bytes Availible
+
+statvfs = os.statvfs(myPath)
+
+freeBytes = statvfs.f_frsize * statvfs.f_bfree
+
+print freeBytes
+
+##Then  we Hand off (freeBytes) to sudo-rand text process.
+
+
+
+seed = "1092384956781341341234656953214543219"
+words = open("lorem.txt", "r").read().replace("\n", '').split()
+fdata(myPath)
+fdata():
+    a = collections.deque(words)
+    b = collections.deque(seed)
+    while True:
+        yield ' '.join(list(a)[0:1024])
+        a.rotate(int(b[0]))
+        b.rotate(1)
+
+g = fdata()
+size = freeBytes # This should be equal to the drive space free. 
+fname = "Omnomnom.nom"
+fh = open(fname, 'w')
+while os.path.getsize(fname) < size:
+    fh.write(g.next())
+## Then we need to call the wiping Utility once again. 
 
